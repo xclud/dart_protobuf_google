a57f5628cbcac78bb955a404be2c6b9e7eda00c3
Mahdi K. Fard <mkfard@outlook.com>
2021-12-20 15:00:27 +0330

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+Public interface definitions of Google APIs for Dart and gRPC.
+
+## Getting started
+
+In your `pubspec.yaml` file add:
+
+```dart
+dependencies:
+  protobuf_google: any
+```
+
+## Usage
+
+```dart
+import 'package:protobuf_google/protobuf_google.dart'
+```
+
---
+++ lib/protobuf_google.dart
@@ -0,0 +1,53 @@
+library protobuf_google;
+
+export 'src/protobuf/any.pb.dart';
+export 'src/protobuf/any.pbenum.dart';
+export 'src/protobuf/any.pbjson.dart';
+
+export 'src/protobuf/api.pb.dart';
+export 'src/protobuf/api.pbenum.dart';
+export 'src/protobuf/api.pbjson.dart';
+
+export 'src/protobuf/descriptor.pb.dart';
+export 'src/protobuf/descriptor.pbenum.dart';
+export 'src/protobuf/descriptor.pbjson.dart';
+
+export 'src/protobuf/duration.pb.dart';
+export 'src/protobuf/duration.pbenum.dart';
+export 'src/protobuf/duration.pbjson.dart';
+
+export 'src/protobuf/empty.pb.dart';
+export 'src/protobuf/empty.pbenum.dart';
+export 'src/protobuf/empty.pbjson.dart';
+
+export 'src/protobuf/field_mask.pb.dart';
+export 'src/protobuf/field_mask.pbenum.dart';
+export 'src/protobuf/field_mask.pbjson.dart';
+
+export 'src/protobuf/source_context.pb.dart';
+export 'src/protobuf/source_context.pbenum.dart';
+export 'src/protobuf/source_context.pbjson.dart';
+
+export 'src/protobuf/struct.pb.dart';
+export 'src/protobuf/struct.pbenum.dart';
+export 'src/protobuf/struct.pbjson.dart';
+
+export 'src/protobuf/timestamp.pb.dart';
+export 'src/protobuf/timestamp.pbenum.dart';
+export 'src/protobuf/timestamp.pbjson.dart';
+
+export 'src/protobuf/type.pb.dart';
+export 'src/protobuf/type.pbenum.dart';
+export 'src/protobuf/type.pbjson.dart';
+
+export 'src/protobuf/wrappers.pb.dart';
+export 'src/protobuf/wrappers.pbenum.dart';
+export 'src/protobuf/wrappers.pbjson.dart';
+
+export 'src/api/annotations.pb.dart';
+export 'src/api/annotations.pbenum.dart';
+export 'src/api/annotations.pbjson.dart';
+
+export 'src/api/http.pb.dart';
+export 'src/api/http.pbenum.dart';
+export 'src/api/http.pbjson.dart';
