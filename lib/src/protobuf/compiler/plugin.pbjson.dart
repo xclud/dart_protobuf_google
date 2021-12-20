9fddaee8fac4b7e31db6ba025154dcb57955138c
Mahdi K. Fard <mkfard@outlook.com>
2021-12-20 17:18:59 +0330

    Format dart files.

--- lib/src/protobuf/compiler/plugin.pbjson.dart
+++ lib/src/protobuf/compiler/plugin.pbjson.dart
@@ -8,6 +8,7 @@
 import 'dart:core' as $core;
 import 'dart:convert' as $convert;
 import 'dart:typed_data' as $typed_data;
+
 @$core.Deprecated('Use versionDescriptor instead')
 const Version$json = const {
   '1': 'Version',
@@ -20,27 +21,62 @@ const Version$json = const {
 };
 
 /// Descriptor for `Version`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List versionDescriptor = $convert.base64Decode('CgdWZXJzaW9uEhQKBW1ham9yGAEgASgFUgVtYWpvchIUCgVtaW5vchgCIAEoBVIFbWlub3ISFAoFcGF0Y2gYAyABKAVSBXBhdGNoEhYKBnN1ZmZpeBgEIAEoCVIGc3VmZml4');
+final $typed_data.Uint8List versionDescriptor = $convert.base64Decode(
+    'CgdWZXJzaW9uEhQKBW1ham9yGAEgASgFUgVtYWpvchIUCgVtaW5vchgCIAEoBVIFbWlub3ISFAoFcGF0Y2gYAyABKAVSBXBhdGNoEhYKBnN1ZmZpeBgEIAEoCVIGc3VmZml4');
 @$core.Deprecated('Use codeGeneratorRequestDescriptor instead')
 const CodeGeneratorRequest$json = const {
   '1': 'CodeGeneratorRequest',
   '2': const [
-    const {'1': 'file_to_generate', '3': 1, '4': 3, '5': 9, '10': 'fileToGenerate'},
+    const {
+      '1': 'file_to_generate',
+      '3': 1,
+      '4': 3,
+      '5': 9,
+      '10': 'fileToGenerate'
+    },
     const {'1': 'parameter', '3': 2, '4': 1, '5': 9, '10': 'parameter'},
-    const {'1': 'proto_file', '3': 15, '4': 3, '5': 11, '6': '.google.protobuf.FileDescriptorProto', '10': 'protoFile'},
-    const {'1': 'compiler_version', '3': 3, '4': 1, '5': 11, '6': '.google.protobuf.compiler.Version', '10': 'compilerVersion'},
+    const {
+      '1': 'proto_file',
+      '3': 15,
+      '4': 3,
+      '5': 11,
+      '6': '.google.protobuf.FileDescriptorProto',
+      '10': 'protoFile'
+    },
+    const {
+      '1': 'compiler_version',
+      '3': 3,
+      '4': 1,
+      '5': 11,
+      '6': '.google.protobuf.compiler.Version',
+      '10': 'compilerVersion'
+    },
   ],
 };
 
 /// Descriptor for `CodeGeneratorRequest`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List codeGeneratorRequestDescriptor = $convert.base64Decode('ChRDb2RlR2VuZXJhdG9yUmVxdWVzdBIoChBmaWxlX3RvX2dlbmVyYXRlGAEgAygJUg5maWxlVG9HZW5lcmF0ZRIcCglwYXJhbWV0ZXIYAiABKAlSCXBhcmFtZXRlchJDCgpwcm90b19maWxlGA8gAygLMiQuZ29vZ2xlLnByb3RvYnVmLkZpbGVEZXNjcmlwdG9yUHJvdG9SCXByb3RvRmlsZRJMChBjb21waWxlcl92ZXJzaW9uGAMgASgLMiEuZ29vZ2xlLnByb3RvYnVmLmNvbXBpbGVyLlZlcnNpb25SD2NvbXBpbGVyVmVyc2lvbg==');
+final $typed_data.Uint8List codeGeneratorRequestDescriptor = $convert.base64Decode(
+    'ChRDb2RlR2VuZXJhdG9yUmVxdWVzdBIoChBmaWxlX3RvX2dlbmVyYXRlGAEgAygJUg5maWxlVG9HZW5lcmF0ZRIcCglwYXJhbWV0ZXIYAiABKAlSCXBhcmFtZXRlchJDCgpwcm90b19maWxlGA8gAygLMiQuZ29vZ2xlLnByb3RvYnVmLkZpbGVEZXNjcmlwdG9yUHJvdG9SCXByb3RvRmlsZRJMChBjb21waWxlcl92ZXJzaW9uGAMgASgLMiEuZ29vZ2xlLnByb3RvYnVmLmNvbXBpbGVyLlZlcnNpb25SD2NvbXBpbGVyVmVyc2lvbg==');
 @$core.Deprecated('Use codeGeneratorResponseDescriptor instead')
 const CodeGeneratorResponse$json = const {
   '1': 'CodeGeneratorResponse',
   '2': const [
     const {'1': 'error', '3': 1, '4': 1, '5': 9, '10': 'error'},
-    const {'1': 'supported_features', '3': 2, '4': 1, '5': 4, '10': 'supportedFeatures'},
-    const {'1': 'file', '3': 15, '4': 3, '5': 11, '6': '.google.protobuf.compiler.CodeGeneratorResponse.File', '10': 'file'},
+    const {
+      '1': 'supported_features',
+      '3': 2,
+      '4': 1,
+      '5': 4,
+      '10': 'supportedFeatures'
+    },
+    const {
+      '1': 'file',
+      '3': 15,
+      '4': 3,
+      '5': 11,
+      '6': '.google.protobuf.compiler.CodeGeneratorResponse.File',
+      '10': 'file'
+    },
   ],
   '3': const [CodeGeneratorResponse_File$json],
   '4': const [CodeGeneratorResponse_Feature$json],
@@ -51,7 +87,13 @@ const CodeGeneratorResponse_File$json = const {
   '1': 'File',
   '2': const [
     const {'1': 'name', '3': 1, '4': 1, '5': 9, '10': 'name'},
-    const {'1': 'insertion_point', '3': 2, '4': 1, '5': 9, '10': 'insertionPoint'},
+    const {
+      '1': 'insertion_point',
+      '3': 2,
+      '4': 1,
+      '5': 9,
+      '10': 'insertionPoint'
+    },
     const {'1': 'content', '3': 15, '4': 1, '5': 9, '10': 'content'},
   ],
 };
@@ -66,4 +108,5 @@ const CodeGeneratorResponse_Feature$json = const {
 };
 
 /// Descriptor for `CodeGeneratorResponse`. Decode as a `google.protobuf.DescriptorProto`.
-final $typed_data.Uint8List codeGeneratorResponseDescriptor = $convert.base64Decode('ChVDb2RlR2VuZXJhdG9yUmVzcG9uc2USFAoFZXJyb3IYASABKAlSBWVycm9yEi0KEnN1cHBvcnRlZF9mZWF0dXJlcxgCIAEoBFIRc3VwcG9ydGVkRmVhdHVyZXMSSAoEZmlsZRgPIAMoCzI0Lmdvb2dsZS5wcm90b2J1Zi5jb21waWxlci5Db2RlR2VuZXJhdG9yUmVzcG9uc2UuRmlsZVIEZmlsZRpdCgRGaWxlEhIKBG5hbWUYASABKAlSBG5hbWUSJwoPaW5zZXJ0aW9uX3BvaW50GAIgASgJUg5pbnNlcnRpb25Qb2ludBIYCgdjb250ZW50GA8gASgJUgdjb250ZW50IjgKB0ZlYXR1cmUSEAoMRkVBVFVSRV9OT05FEAASGwoXRkVBVFVSRV9QUk9UTzNfT1BUSU9OQUwQAQ==');
+final $typed_data.Uint8List codeGeneratorResponseDescriptor = $convert.base64Decode(
+    'ChVDb2RlR2VuZXJhdG9yUmVzcG9uc2USFAoFZXJyb3IYASABKAlSBWVycm9yEi0KEnN1cHBvcnRlZF9mZWF0dXJlcxgCIAEoBFIRc3VwcG9ydGVkRmVhdHVyZXMSSAoEZmlsZRgPIAMoCzI0Lmdvb2dsZS5wcm90b2J1Zi5jb21waWxlci5Db2RlR2VuZXJhdG9yUmVzcG9uc2UuRmlsZVIEZmlsZRpdCgRGaWxlEhIKBG5hbWUYASABKAlSBG5hbWUSJwoPaW5zZXJ0aW9uX3BvaW50GAIgASgJUg5pbnNlcnRpb25Qb2ludBIYCgdjb250ZW50GA8gASgJUgdjb250ZW50IjgKB0ZlYXR1cmUSEAoMRkVBVFVSRV9OT05FEAASGwoXRkVBVFVSRV9QUk9UTzNfT1BUSU9OQUwQAQ==');
